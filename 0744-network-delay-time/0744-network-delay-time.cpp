4270607e960d455f9e1785735dc4af33fb57edf0
vedanth2001 <58515685+vedanth2001@users.noreply.github.com>
2025-02-03 11:26:43 +0530

    Time: 95 ms (32.51%), Space: 48 MB (16.56%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.cpp
+++ 0744-network-delay-time/0744-network-delay-time.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        // Adjacency list to store the graph
-        vector<vector<pair<int, int>>> adj(n + 1);  // n + 1 for 1-based indexing
-        for (auto& time : times) {
-            int u = time[0], v = time[1], w = time[2];
-            adj[u].emplace_back(v, w);
+        vector<vector<pair<int, int>>> adj(n+1);
+        int minTime = 0;
+        for (auto edge : times) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            adj[u].push_back({v, w});
         }
-
-        // Distance vector initialized to INT_MAX, for each node
-        vector<int> dist(n + 1, INT_MAX);  // n + 1 for 1-based indexing
+         vector<int> dist(n+1, INT_MAX);
         dist[k] = 0;
-
-        // Min-heap priority queue to store {distance, node}
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-        pq.push({0, k});  // Start from node `k` with distance 0
-
-        while (!pq.empty()) {
-            auto [currentDist, node] = pq.top();
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({0, k});
+        while(!pq.empty()){
+            int node = pq.top().second;
+            int currDist = pq.top().first;
             pq.pop();
-
-            // Skip processing if the current distance is outdated
-            if (currentDist > dist[node]) continue;
-
-            // Relaxation step for adjacent nodes
-            for (auto& [neighbor, weight] : adj[node]) {
-                if (dist[node] + weight < dist[neighbor]) {
-                    dist[neighbor] = dist[node] + weight;
-                    pq.push({dist[neighbor], neighbor});
+            // calculates distance from current node to this node.
+            for(auto [neighbour, weight] : adj[node]){
+                if(dist[neighbour] > currDist+weight){
+                    dist[neighbour] = currDist+weight;
+                    pq.push({dist[neighbour], neighbour});
                 }
             }
         }
-
-       
-        int res = 0;  
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == INT_MAX) return -1;  // If any node is unreachable
-            res = max(res, dist[i]);
+        for (int i = 1; i <= n; i++) {  
+            if (dist[i] == INT_MAX) return -1;  
+            minTime = max(minTime, dist[i]);
         }
-
-        return res;
+        return minTime;
     }
 };
