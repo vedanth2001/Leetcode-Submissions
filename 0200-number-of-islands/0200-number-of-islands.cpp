5d2d2dd2f1adbde225f9a78b227203e619d414f5
vedanth2001 <58515685+vedanth2001@users.noreply.github.com>
2025-02-04 12:21:57 +0530

    Time: 19 ms (95.87%), Space: 24.5 MB (18.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    void findBfs(vector<vector<char>>& grid, int row, int col, vector<vector<bool>> &vis){
-        vis[row][col] = true;
+    const vector<pair<int, int>> dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+    void processNeighbours(vector<vector<char>>& grid, vector<vector<int>> &visited, int row, int col){
         queue<pair<int, int>> q;
-        int n = grid.size();
-        int m = grid[0].size();
+        visited[row][col] = 1;
         q.push({row, col});
         while(!q.empty()){
             int r = q.front().first;
             int c = q.front().second;
             q.pop();
+            for(auto newDir: dir){
+                int nr = r + newDir.first;
+                int nc = c + newDir.second;
 
-            int dr[4] = {0, -1, 0, 1};
-            int dc[4] = {-1, 0, 1, 0};
-
-
-            for(int i =0;i<4;i++){
-                int nrow = r + dr[i];
-                int ncol = c + dc[i];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m 
-                   && grid[nrow][ncol]=='1' && vis[nrow][ncol] == 0){
-                    q.push({nrow, ncol});
-                    vis[nrow][ncol] = true;
-                } 
+                if(nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() && visited[nr][nc] == 0 && grid[nr][nc] == '1'){
+                q.push({nr, nc});
+                visited[nr][nc] = 1;
+                }
             }
         }
         return;
@@ -31,18 +24,16 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        int count = 0;
-        vector<vector<bool>> vis(n, vector<bool>(m,false));
-        
+        int islands = 0;
+        vector<vector<int>> visited(n, vector<int>(m, 0));
         for(int i =0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(vis[i][j] == false && grid[i][j]=='1'){
-                    findBfs(grid, i, j, vis);
-                    count++;
+            for(int j=0;j<m;j++){
+                if(visited[i][j]==0 && grid[i][j] == '1'){
+                    islands++;
+                    processNeighbours(grid, visited, i, j);
                 }
             }
-        } 
-
-        return count;
+        }
+        return islands;
     }
 };
